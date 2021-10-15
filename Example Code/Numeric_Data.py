1d9aaf0a8966ea8f6851e0df7ae80d9aa5e07bb0
William Kelly <wfkelly12@yahoo.com>
2021-10-14 21:51:48 -0700

    Updated Numeric
    Added in Types of Operations and Increment

--- Example Code/Numeric_Data.py
+++ Example Code/Numeric_Data.py
@@ -1,1 +1,18 @@
 # Introduction to Integer and Float Numeric Data
+
+num = 3.14
+print(type(num)) # Type Float, Int
+
+print(3+2) # Addition
+print(3-2) # Subtraction
+print(3*2) # Multiplication
+print(3/2) # Division
+print(3**2) # Exponent
+print(3%2) # Modulus
+print(abs(-3)) # Absolute Value
+print(3 * (2 + 1)) # Order of operations
+
+# Increment Numeric
+num = 1
+num += 1
+print(num)
